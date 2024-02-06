97e63625ac8df010981c4bead1326c83c1c21175
0xGuigui <guilhem.jeh@gmail.com>
2024-02-06 15:48:17 +0100

    Supprimer des éléments de la configuration

--- src/custom_delete.py
+++ src/custom_delete.py
@@ -3,3 +3,28 @@
 ##  SweePy
 ##
 
+from include.imports import ctypes, os, sys, messagebox, shutil, glob, configparser
+
+
+def delete_elements_from_config(file_path):
+    # Créer une instance de ConfigParser
+    config = configparser.ConfigParser()
+
+    # Lire le fichier de configuration
+    config.read(file_path)
+
+    # Parcourir toutes les sections
+    for section in config.sections():
+        # Parcourir tous les éléments de la section
+        for key, value in config.items(section):
+            # Remplacer %USER% par le nom de l'utilisateur actuel
+            value = os.path.expanduser(value.replace('%USER%', '~'))
+
+            # Si la clé est 'F', alors supprimer le fichier
+            if key.upper() == 'F':
+                if os.path.isfile(value):
+                    os.remove(value)
+            # Si la clé est 'P', alors supprimer le dossier
+            elif key.upper() == 'P':
+                if os.path.isdir(value):
+                    shutil.rmtree(value)
